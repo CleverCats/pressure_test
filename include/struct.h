3b95c80cad29034599b82b6721df618e1c673397
gmc <1449971289@qq.com>
2023-07-27 08:16:12 +0800

    规范代码及注释

--- include/struct.h
+++ include/struct.h
@@ -1,17 +1,19 @@
 #pragma once
 
-#pragma pack(1)   
+#pragma pack(1)
 /* 包头结构 */
-typedef struct{
+typedef struct
+{
     unsigned short packageLength; // 报文长度【包头+包体】
     unsigned short messageCode;   // 消息类型代码【区别命令类型】
     int crc32;                    // 校验是否为过期包
 } COMM_PKG_HEADER, *LP_COMM_PKG_HEADER;
 
 /*登录*/
-typedef struct {
-    char           Registercid[50];      //登录账户
-    char           Registerccode[50];    //登录密码
-}Register,*LP_Register;
+typedef struct
+{
+    char Registercid[50];   // 登录账户
+    char Registerccode[50]; // 登录密码
+} Register, *LP_Register;
 
-#pragma pack() 
+#pragma pack()
